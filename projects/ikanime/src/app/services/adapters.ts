4a0e485e7c765998453057dbb5fc05ec14e7663e
IkedaHarket <sebaaignacio111@gmail.com>
2024-02-24 11:20:15 -0300

    DEV: Pagination Response adapted

---
+++ projects/ikanime/src/app/services/adapters.ts
@@ -0,0 +1,14 @@
+import { PaginationResponse } from "../interfaces/api"
+import { Pagination } from "../models"
+
+
+export function convertToPagination<T>(paginationResponse: PaginationResponse<T>): Pagination<T>{
+    return new Pagination<T>({
+    limit: paginationResponse.limit,
+    next: paginationResponse.next,
+    page: paginationResponse.page,
+    prev: paginationResponse.prev,
+    records: paginationResponse.records,
+    total: paginationResponse.total,
+    })
+}
