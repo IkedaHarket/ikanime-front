a6c5be3e379f5d703c8df48ce8db8f8fe64f07bd
IkedaHarket <sebaaignacio111@gmail.com>
2024-02-25 10:21:45 -0300

    DEV: Multiselect lib created

---
+++ projects/web-components/src/lib/multiselect/multiselect.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'web-multiselect',
+  standalone: true,
+  imports: [],
+  templateUrl: './multiselect.component.html'
+})
+export class MultiselectComponent {
+
+}
