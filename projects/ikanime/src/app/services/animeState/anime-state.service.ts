3cace31965af7f0723ef4e98151e1f7d86abef96
IkedaHarket <sebaaignacio111@gmail.com>
2024-02-27 17:34:10 -0300

    DEV: animeState service OK!

--- projects/ikanime/src/app/services/animeState/anime-state.service.ts
+++ projects/ikanime/src/app/services/animeState/anime-state.service.ts
@@ -1,9 +1,48 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from 'projects/ikanime/src/environments/environment';
+import * as API_INTERFACES from '../../interfaces/api';
+import * as API_MODELS from '../../models/api';
+import * as Rxjs from 'rxjs';
+import { Pagination } from '../../models';
+import { AnimeState } from '../../models/anime';
+import { convertToPagination } from '../adapters';
+import { convertToState } from './adapter';
 
+interface FindOptions{
+  queries?: {
+    page: number,
+    limit: number
+  },
+}
 @Injectable({
   providedIn: 'root'
 })
 export class AnimeStateService {
 
-  constructor() { }
+  private _http = inject(HttpClient)
+  private _baseUrl = environment.API_URL
+
+  find(options: FindOptions = {}): Rxjs.Observable<Pagination<AnimeState[]> | null>{
+    let url = `${this._baseUrl}/anime/state/find`;
+    if(options.queries){
+      url += `?${ new API_MODELS.PaginationRequest(options.queries.page, options.queries.limit).createUrlParams() }`
+    }
+    
+    return this._http.post<API_INTERFACES.ServerResponse<API_INTERFACES.PaginationResponse<API_INTERFACES.StateFindResponse[]>>>(
+      url, {}
+      ).pipe(
+          Rxjs.map( ({ response }) => {
+            return convertToPagination<AnimeState[]>({
+              ...response,
+              records: convertToState(response.records)
+            })
+          } ),
+          Rxjs.catchError( (e)=>  this._handleError(e) ),
+        );
+  }
+
+  private _handleError(error: HttpErrorResponse) {
+    return Rxjs.of(null)
+  }
 }
