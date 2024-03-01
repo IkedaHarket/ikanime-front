3eac99b44acd1694fd36187d8f0e1e9f17e7db13
IkedaHarket <sebaaignacio111@gmail.com>
2024-03-01 16:25:59 -0300

    DEV: ClickOutside multiselect OK

--- projects/web-components/src/lib/components/multiselect/multiselect.component.ts
+++ projects/web-components/src/lib/components/multiselect/multiselect.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { ClickOutsideDirective } from '../../directives/clickOutside/click-outside.directive';
 
 interface Inputs {
   label: string,
@@ -8,16 +9,19 @@ interface Inputs {
 @Component({
   selector: 'web-multiselect',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [ CommonModule, ClickOutsideDirective ],
   templateUrl: './multiselect.component.html'
 })
 export class MultiselectComponent {
   @Input({ required: true }) props !: Inputs
   
-  public hasClick: boolean = true
+  public hasClick: boolean = false
 
   toggleHasClick(): void {
     this.hasClick = !this.hasClick
   }
 
+  handleClickOutside(){
+    this.hasClick = false
+  }
 }
