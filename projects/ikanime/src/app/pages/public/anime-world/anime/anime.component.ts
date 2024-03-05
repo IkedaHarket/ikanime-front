19c5b0723bc14ca5d145e0b17d08ae3e76a5ba6d
IkedaHarket <sebaaignacio111@gmail.com>
2024-03-05 13:25:07 -0300

    DEV: Header anime view

--- projects/ikanime/src/app/pages/public/anime-world/anime/anime.component.ts
+++ projects/ikanime/src/app/pages/public/anime-world/anime/anime.component.ts
@@ -1,21 +1,37 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import * as WebComponents from '@webComponents';
 import { AnimeService } from 'projects/ikanime/src/app/services';
 
 @Component({
   selector: 'app-anime',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    WebComponents.BadgeAnimeCategoryComponent,
+    WebComponents.BadgeAnimeTypeComponent,
+    WebComponents.ButtonComponent,
+  ],
   templateUrl: './anime.component.html'
 })
 export class AnimeComponent implements OnInit{
 
   private _animeService = inject(AnimeService)
+  private _route = inject(ActivatedRoute)
 
+  public anime = this._animeService.animes
+  
   ngOnInit(): void {
-    this._animeService.findAnimeBuilder().setFindFilters({
-      
-    })
-    
+    this._route.params.subscribe((params)=>{
+      this._animeService.findAnimeBuilder().setFindFilters({
+        body:{
+          uniqueName:{
+            contains: params['name']
+          }
+        }
+      }).setAnimes(true).build().subscribe()
+    })    
   }
 
 }
